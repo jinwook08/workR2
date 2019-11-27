a424ba46effe1000ae3a2e6e2e101a9a6c8b2d6d
jinwook08 <ckwlsdnr08@naver.com>
2019-11-27 15:28:12 +0900

    Update first.R

--- first.R
+++ first.R
@@ -32,14 +32,113 @@ cat("string:", str_value,"\n")
 cat("Boolean value:", booleanvalue,"\n")
 
 
-numberValue <- scan()       #input기능입니다.//쓰기 기능입니다.  
+numberValue <- scan()      #input기능입니다.//쓰기 기능입니다.  
 cat("Numeric number:", numberValue,"\n" )
 
 
+number1 <- 10
+number2 <- 20
+resultAdd <- number1 + number2  #더하기 입니다.
+resultSub <- number1 - number2  #빼기 입니다. 
+resultMul <- number1 * number2  #곱 입니다. 
+resultDiv <- number1 / number2  # 나누기 입니다. 
+resultRem <- number2 %% number1  #나머지 입니다. 
+resultSec <- number2 ^ 5         #제곱근 입니다. 
+resultSec2 <-number2 **5
+print(resultAdd)
+print(resultSub)
+print(resultMul)
+print(resultDiv)
+print(resultRem)
+print(resultSec)
+print(resultSec2)
 
 
 
 
+number1 <- 0
+number1 <- number1 +10
+number1
+#
+
+number1 <- 0
+number1 <- number1 +10
+number1
+number1 <- 0
+number1 <- number1 +10
+number1
+
+number1 <- 0
+number1 <- number1 +10
+number1
+
+number1 <- 0
+number1 <- number1 +10
+number1
+number1 <- number1 +10
+number1
+number1 <- number1 +10
+number1
+
+number1 <- 0
+number1 <- number1 +10
+number1
+number1 <- number1 +10
+number1
+number1 <- number1 +10
+number1
+number1 <- number1 +10
+number1
+number1 <- number1 +10
+number1
+number1 <- number1 +10
+number1
+
+
+number2 <-100
+number1 <- number2 +10
+number1
+number2
+
+print(number1 + 10 * number2 /2)
+
+print((umber1 + 10)* number2 /2)
+
+
+number300 <- 10
+number300 <- number300 + 10
+number300
+
+
+number301 <-  number300 *2
+number301
+
+
+number1 <- 10.5
+number2 <- 10
+print(number1 > number2)
+print(number1 >= number2)
+print(number1 < number2)
+print(number1 >= number2)
+print(number1 == number2)
+print(number1 != number2)
+
+print(number1 > 10 & number2 >10)         #AND
+print(number1 > 10 | number2 > 10)        #OR
+print(!(number1 > 10))                     #NOT
+
+
+number <- 100 
+str <- "R Language"
+result = number + str 
+print(result)
+
+
+
+number <- "100" 
+str <- "R Language"
+result = number + str 
+print(result)
 
 
 
