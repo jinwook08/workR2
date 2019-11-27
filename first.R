6f2be413c98eb0fefc2c347a3e69d6ee289e71fe
jinwook08 <ckwlsdnr08@naver.com>
2019-11-27 18:22:29 +0900

    Update first.R

--- first.R
+++ first.R
@@ -141,4 +141,170 @@ result = number + str
 print(result)
 
 
+#제어구조 -선택구조
+
+job.type <- 'A'        #A라는 문자 저장
+
+#
+if (job.type == 'B'){              
+  bonus <- 200
+}else{
+  bonus <- 100
+}
+cat('joy type :', job.type, "\t\tbonus:",bonus)
+
+
+
+###############################################
+
+job.type <- 'B'
+
+if (job.type == 'A'){
+  bonus <- 200
+}
+cat("joy type :", job.type, "\t\tbonus :", bonus)
+
+
+
+
+score <- 85
+
+if (score >= 90){
+  grade <- 'A'
+} else if (score >= 80){
+  grade <- 'B'
+}else if (score>=70){
+  grade <- 'C'
+}else if (score>=60){
+  grade <- 'D'
+}else{
+  grade <- 'F'    
+}
+cat("score:",score,"\t\tgrade:",grade)
+
+
+
+
+
+number <- 29
+
+if (number %% 2 == 0){
+  num <- '짝수입니다.'
+}else {
+  num <- '홀수입니다.'
+}
+
+cat("number:",number,"는",num)
+
+
+
+
+
+a <- 10 
+b <- 20
+if (a> 5& b>5){
+  cat(a,">5 and ", b, ">5")
+}else{
+  cat(a, "<=5 or ", b, "<= 5")
+}
+
+
+
+
+a <- 4
+b <- 20
+if (a> 5& b>5){
+  cat(a,">5 and ", b, ">5")
+}else{
+  cat(a, "<=5 or ", b, "<= 5")
+}
+
+
+
+a <- 10
+b <- 20
+
+if (a>b){
+  c <- a
+}else{
+  c <- b
+}
+cat("a= ", a, "\tb= ",b,"\tc=",c)
+
+
+c <- ifelse(a>b,a,b)
+cat("a= ", a, "\tb= ",b,"\tc=",c)
+
+help(ifelse)
+
+
+x <- c(6:-4)
+print(x)
+
+#맥스값 구하기/. 
+
+a <- 900
+b <- 102
+c <- 242
+max <-a
+  
+if (b>max){
+  max <- b
+}
+if(c>max){
+  max <- c
+}
+cat("a=",a,"b=",b,"c=",c,"max=",max)
+
+###################################
+#반복구조
+
+for (i in 1:10){
+  a=i+i
+  cat("쓰레기를",a,"개 주웠습니다.")
+  print(a)
+}
+
+
+
+multiple = 2 
+for (i in 1:9 ){
+  cat(multiple,'X',i,'=',multiple * i, '\n')
+}
+
+
+
+# while
+i <- 1
+while (i<=10){
+  print(i)
+  i <- i+1
+}
+
+multiple = 2 
+i <- 1
+while(i<=9){
+  cat(multiple,'X',i,'=',multiple * i, '\n')
+  i <- i +1
+}
+
+#10개씩 끊어서 만들기.
+
+lin <- 1
+for (i in 1:100){
+  cat(i, ' ')
+  lin <- lin + 1
+  if (lin >10){
+    print(' \n')
+    lin <- 1
+  }
+}
+
+
+for (i in 1:100){
+  cat( i, ' ')
+  if(i %% 10 == 0){
+    print('\n')
+  }
+}
 
