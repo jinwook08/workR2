e5b0aeb64ac7e34a50403a435f215a87f7fe0433
jinwook08 <ckwlsdnr08@naver.com>
2019-11-28 17:16:23 +0900

    Update mylib.R

--- mylib.R
+++ mylib.R
@@ -22,7 +22,7 @@ my_max <- function(x,y){
 
 #사용자 정의 함수 매개변수 초기값 설정
 my_div <- function(x,y=2){   #y의 초기값이 주어지지 않으면 2로 설정
-  result <- x/y
+  result <- x/y               #초기값은 반드시 뒤에 값을 지정해야한다.
   
   return(result)
 }
