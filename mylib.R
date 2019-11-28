967f2a2406bdae05b4348588a5becda67c4db0da
jinwook08 <ckwlsdnr08@naver.com>
2019-11-28 11:52:07 +0900

    Create mylib.R

---
+++ mylib.R
@@ -0,0 +1,37 @@
+##
+## mylib.R
+##
+##이런게 나중에 package가 된다. 이게 바로 라이브러리 파일이다. 이런 
+#라이브러리만 넣어 논다. 
+
+##
+##사용자 정의함수
+##최대값 찾기.
+
+my_max <- function(x,y){
+  num.max <- x
+  if (y>num.max){
+    num.max <- y
+  }
+  return(num.max)
+}
+
+
+
+
+
+#사용자 정의 함수 매개변수 초기값 설정
+my_div <- function(x,y=2){   #y의 초기값이 주어지지 않으면 2로 설정
+  result <- x/y
+  
+  return(result)
+}
+
+
+
+
+
+
+
+
+
